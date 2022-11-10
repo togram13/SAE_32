d71c7ea23883f1295fcbfa6b6dd68f10890e84ea
margot <margot.bonafos@gmail.com>
2022-11-10 19:16:20 +0100

    Video 1 scroll OK

---
+++ codes/scroll/video1/recepteur1/recepteur1.ino
@@ -0,0 +1,145 @@
+
+#include <SPI.h> //Nativement dans arduino donc pas besoin de l'installer en +
+#include <RH_RF95.h> //gestion des transmissions radio en Lora
+#include <M5Stack.h>
+
+#define RFM95_CS 5 //M5Stack Lora
+#define RFM95_DIO0 36
+RH_RF95 rf95(RFM95_CS, RFM95_DIO0);
+
+uint8_t etat; //etat courant codé sur 1 octet
+uint8_t rxdata[16]; //Tableau du buffer de reception
+uint8_t rxdatataille = 16; // Taille Max du buffer en reception
+uint8_t rxlendata = 16; //Taille de la trame reçue
+int rxNbTrame; //nombre de trames reçues
+
+#define ECOUTE 1
+#define TEST_RX 2
+#define AFFICHAGE 3
+
+#define canal 0 // Identification du même canal que pour l'émission, pour la réception
+
+void setup() {
+  Serial.begin(115200);
+  M5.begin();
+  M5.Power.begin(); //initialisation de la puissance
+  termInit();
+  M5.Lcd.setTextFont(4);
+
+  char str[255] = "Video DATA Point a point\r ";
+  printString(str);
+  termPutchar('\r');
+
+  strcpy(str,"On commence\r");
+  printString(str);
+  termPutchar('\r');
+
+  //initialisation du transceiver radio:
+
+  if(!rf95.init()){ //init règle la puissance à 13dBm
+    strcpy(str,"Erreur initialisation RF95\r");  // on vérifie si il y a une erreur avec le transceiver
+    printString(str);
+    termPutchar('\r');
+  }                           // Problèmes possibles : cablages ou drivers logiciels
+  else{
+    strcpy(str,"Initialisation OK\r");
+    printString(str);
+    termPutchar('\r');
+  }
+
+      //configuration des parametres Radio :
+  rf95.setModemConfig(RH_RF95::Bw125Cr45Sf128); //Selectionne des config de modems prédéfinis
+  rf95.setFrequency(867.7); //règle la fréquence centrale Bande
+
+  etat = ECOUTE; //état de départ(cf Automate récepteur)
+  delay(1000);
+  strcpy(str,"boucle principale\r");
+  printString(str); // Message du début, qui nous indique la fin des config  de début
+  termPutchar('\r');
+  delay(1000);
+  rxNbTrame = 0; // Initialisation à 0 du nombre de trames reçues
+}
+
+void loop() {
+
+  char temp[500];
+  char octet[50];
+  char str[255] = "Video DATA Point a point\r ";
+  switch(etat)//Le switch permet de réagir en fonction de différents cas (ici les valeurs de etat)
+  {
+      case ECOUTE: // etat d'ecoute(etat de départ)
+        termPutchar('\r');
+        strcpy(str,"En ECOUTE\r");
+        printString(str);
+        termPutchar('\r');
+
+        rf95.setModeRx(); // On met le transceiver en mode réception
+        
+        etat = TEST_RX; // On passe à l'état d'après (Etat 2)
+        break;
+         
+       case TEST_RX: //etat de test : boucle qui test si il y des données dans le buffer
+        //delay(1000);
+          //M5.Lcd.print("Test du buffer\n");//affichage de l'état en cours
+          //termPutchar('\r');
+          
+          //M5.Lcd.print(rf95.available());
+          if(rf95.available()) // Si il y a des choses dans le buffer => execute le code du if
+          {
+
+            strcpy(str,"Il y a quelque chose dans le buffer\r");
+            printString(str);
+            termPutchar('\r');
+            etat = AFFICHAGE;
+          }
+          break;
+        
+      case AFFICHAGE : //On passe en état d'affichage 
+        if(rf95.recv(rxdata, &rxlendata)) // On récupère les datas dans le buffer, en lui donnant en paramètres le buffer de réception (rxdata) et on passe par reference rxlendata qui contient la taille Max de la trame reçue (j'ai mis 16) 
+        //La valeur de rxlendata changera en 2 car le buffer ne contiendra que 2 octets quelque soit l'envoi
+        {
+          rxNbTrame++; // on incrémente le nombre de trames reçues de 1
+           // Affichage du nombre de trames reçues 
+          strcpy(str,"Nombre de trame :  ");
+          printString(str);
+
+          sprintf(temp, "%d", rxNbTrame);
+          printString(temp);
+          termPutchar('\r');
+
+           // Affichage du nombres d'octets reçus
+          strcpy(str,"nombre d'octets recu :  ");
+          printString(str);
+
+          sprintf(temp, "%u", rxlendata);
+          printString(temp);
+          termPutchar('\r');
+          termPutchar('\r');
+
+          for(int i=0; i<rxlendata; i++)
+          {
+            strcpy(str,"octet ");
+            printString(str);
+
+            sprintf(octet, "%u", i+1);
+            printString(octet);
+
+            strcpy(str," :  ");
+            printString(str);
+
+            // Affichage du contenu de la trame
+            sprintf(temp, "%u", rxdata[i]);
+            printString(temp);
+            termPutchar('\r');
+            termPutchar('\r');
+          }
+        termPutchar('\r');
+        
+      }
+      etat = ECOUTE;
+      break;
+
+      default: // Par précaution, on prévoit au switch un cas par défaut
+        break;
+  }
+}
