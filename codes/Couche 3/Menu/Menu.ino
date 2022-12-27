0d71b2c3dff561daeefb1916c22d3a4acb7c7acb
lilian-breux <105793062+lilian-breux@users.noreply.github.com>
2022-12-27 14:52:32 +0100

    Vrai version fonctionnelle de l'affichage pour le menu
    Le retour à la réalité pour Lilian ... Ce code qui m'a pris tellement de temps à régler est enfin fonctionnel (parti affichage).

--- codes/Couche 3/Menu/Menu.ino
+++ codes/Couche 3/Menu/Menu.ino
@@ -2,113 +2,198 @@
 #include <RH_RF95.h>
 #include <M5Stack.h>
 
-//Fonction d'affichage
-void affichage(bool menu_data, uint16_t etat_menu, uint16_t freq, uint16_t choixTTL, uint16_t ipm5, uint16_t valTTL, uint16_t status_send){
-  if (menu_data=0) {
-    Serial.print("Etat initial");
-  }
-  else {
-    if (choixTTL == 0){
-      message_choix_freq(etat_menu, freq);
-      message_choix_TTL(etat_menu, choixTTL);
-      message_envoie(etat_menu, status_send);
-    }
-    if (choixTTL == 1){
-      message_choix_freq(etat_menu, freq);
-      message_choix_TTL(etat_menu, choixTTL);
-      message_choix_dest(etat_menu, ipm5);
-      message_envoie(etat_menu, status_send);
-    }
-    if (choixTTL == 2){
-      message_choix_freq(etat_menu, freq);
-      message_choix_TTL(etat_menu, choixTTL);
-      message_choix_dest(etat_menu, ipm5);
-      message_choix_valTTL(etat_menu, valTTL);
-      message_envoie(etat_menu, status_send);
-    }
-  }
+#define BASE_FREQ 867.7
+#define BASE_CHOIX_TTL 0
+#define BASE_IP_DEST 5
+#define BASE_VAL_TTL 7
+
+#define MAX_FREQ 1000
+#define MIN_FREQ 600
+#define PAS_FREQ 10 //Valeur d'ajout/de soustraction de fréquance avec les boutons B et C
+
+#define NOMBRE_DESTINATAIRES 20 //Nombre maximum d'adresse dans le réseau
+
+#define TTL_MAX 32 //Nombre TTL max
+#define SELF_IP 4 //Ip du M5
+
+char text[255];
+
+uint8_t menu_data = 0; //état du menu : 
+  //Si 0 alors menu de base avec juste écrit l'ip 
+  //Si 1 alors dans le menu d'envoie
+uint16_t etat_menu = 0; //état de modification du menu : 
+  //Si 0 alors choix de frequence 
+  //Si 1 Choix TTL 
+  //Si 2 Choix du destinataire de TTL 
+  //Si 3 Choix du nombre de TTL 
+  //Si 4 envoie du packet
+
+uint16_t freq=BASE_FREQ;
+uint16_t choixTTL=0;
+uint16_t ipm5=BASE_IP_DEST;
+uint16_t valTTL=BASE_VAL_TTL;
+uint16_t status_send=0;
+
+uint16_t self_ip=4; //Valeur à changer par la vraie IP de notre M5 Stack
+
+void Fonction_envoie_data_noTTL_noDest(uint16_t freq, uint16_t choixTTL){
+  M5.Lcd.clear(BLACK);
+}
+void Fonction_envoie_data_noTTL_Dest(uint16_t freq, uint16_t choixTTL, uint16_t ipm5, uint16_t self_ip){
+  M5.Lcd.clear(BLACK);
+}
+void Fonction_envoie_data_TTL_Dest(uint16_t freq, uint16_t choixTTL, uint16_t ipm5, uint16_t valTTL, uint16_t self_ip){
+  M5.Lcd.clear(BLACK);
 }
 
-void message_choix_freq(uint16_t etat_menu, uint16_t freq){
-  if (etat_menu == 0) {
-    Serial.printf("Frequance : <--",freq);
-    Serial.printf("-->");
-  }
-  else {
-    Serial.printf("Frequance",freq);
-  }
+void setup(){
+  // put your setup code here, to run once:
+  //Paramètres par défault
+  M5.begin();
+  
+  sprintf(text, "Ip du m5 stack: %d ", SELF_IP);
+  printString(text);
+  termPutchar('\r');
+
 }
 
-void message_choix_TTL(uint16_t etat_menu, uint16_t choixTTL){
-  if (etat_menu == 1){
-    if (choixTTL == 0){
-      Serial.print("Choix TTL : <-- Pas de TTL + Pas de destination -->");
+void loop(){
+  M5.update();
+
+  if(M5.BtnA.isPressed()){ //Si le bouton A est pressé
+    if (menu_data == 0){ //Si on est sur le menu d'acceuil
+      menu_data = 1;
+      affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
     }
-    if (choixTTL == 1){
-      Serial.print("Choix TTL : <-- Pas de TTL + Destination -->");
+    else {//sinon
+      if (etat_menu == 4){ //Moment d'envoyer le paquet
+        if (status_send == 0){ //Correspond à l'état "Envoyer"
+          menu_data = 0;
+          etat_menu = 0;
+          if (choixTTL == 0){
+            Fonction_envoie_data_noTTL_noDest(freq, choixTTL);//Envoie la donnée sans TTL ni destination
+          }
+          if (choixTTL == 1){
+            Fonction_envoie_data_noTTL_Dest(freq, choixTTL, ipm5, self_ip);//Envoie la donnée sans TTL avec destination
+          }
+          if (choixTTL == 2){
+            Fonction_envoie_data_TTL_Dest(freq, choixTTL, ipm5, valTTL, self_ip);//Envoie la donnée avec TTL avec destination
+          }
+          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);// Donc affichage du menu d'acceuil
+        }
+        if (status_send == 1){ //Correspond à l'état "modification"
+          etat_menu = 0;
+          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
+        }
+        else { //Annule les l'envoie de données
+          menu_data = 0;
+          etat_menu = 0;
+          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);// Donc affichage du menu d'acceuil
+        }
+      }
+      else {
+        if (choixTTL == 0 && etat_menu == 1) {
+          etat_menu=4;//Renvoie directement à l'envoie du paquet
+        }
+        if (choixTTL == 1 && etat_menu == 2) {
+          etat_menu=4;//Renvoie directement à l'envoie du paquet
+        }
+        else {
+          if (etat_menu < 4){
+            etat_menu+=1; //On change de configuration
+          }
+        }
+        affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
+      }
     }
-    if (choixTTL == 2){
-      Serial.print("Choix TTL : <-- TTL -->");
-    }
+    delay(1000);
   }
-  else {
-    if (choixTTL == 0){
-      Serial.print("Choix TTL : Pas de TTL + Pas de destination");
+
+  if(M5.BtnB.isPressed()){//Si le bouton B est pressé
+    if (menu_data == 1){//Si la création d'un packet est active
+      if (etat_menu == 0){ //Changement de fréquance
+        if (freq+PAS_FREQ < MAX_FREQ ){
+          freq+=PAS_FREQ;
+          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
+        }
+      }
+      if (etat_menu == 1){ //Choix du type d'envoie
+        if (choixTTL+1 <= 2){
+          choixTTL+=1;
+          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
+        }
+      }
+      if (etat_menu == 2){ //Choix du destinataire
+        if (ipm5+1 == self_ip){
+          if (ipm5+2 <= NOMBRE_DESTINATAIRES){
+            ipm5+=2;
+            affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
+          }
+        }
+        else{
+          if (ipm5+1 <= NOMBRE_DESTINATAIRES){
+            ipm5+=1;
+            affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
+          }
+        }
+      }
+      if (etat_menu == 3){ //Choix du TTL
+        if (valTTL+1 <= TTL_MAX){
+          valTTL+=1;
+          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
+        }
+      }
+      if (etat_menu == 4){ //Choix d'envoie de la donnée
+        if (status_send+1 <= 3){
+          status_send+=1;
+          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
+        }
+      }
     }
-    if (choixTTL == 1){
-      Serial.print("Choix TTL : Pas de TTL + Destination");
+    delay(1000);
+  }
+
+  if(M5.BtnC.isPressed()){//Si le bouton C est pressé
+    if (menu_data == 1){//Si la création d'un packet est active
+      if (etat_menu == 0){ //Changement de fréquance
+        if (freq-PAS_FREQ > MIN_FREQ ){
+          freq-=PAS_FREQ;
+          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
+        }
+      }
+      if (etat_menu == 1){ //Choix du type d'envoie
+        if (choixTTL-1 >= 0){
+          choixTTL-=1;
+          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
+        }
+      }
+      if (etat_menu == 2){ //Choix du destinataire
+        if (ipm5-1 == self_ip){
+          if (ipm5-2 >= 0){
+            ipm5-=2;
+            affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
+          }
+        }
+        else{
+          if (ipm5-1 >= 0){
+            ipm5-=1;
+            affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
+          }
+        }
+      }
+      if (etat_menu == 3){ //Choix du TTL
+        if (valTTL-1 >= 0){
+          valTTL-=1;
+          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
+        }
+      }
+      if (etat_menu == 4){ //Choix d'envoie de la donnée
+        if (status_send-1 >= 0){
+          status_send-=1;
+          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
+        }
+      }
     }
-    if (choixTTL == 2){
-      Serial.print("Choix TTL : TTL");
-    }
+    delay(1000);
   }
 }
-
-void message_choix_dest(uint16_t etat_menu, uint16_t ipm5){
-  if (etat_menu == 2) {
-    Serial.printf("Destination : <--",ipm5);
-    Serial.printf("-->");
-  }
-  else {
-    Serial.printf("Destination :",ipm5);
-  }
-}
-
-void message_choix_valTTL(uint16_t etat_menu, uint16_t valTTL){
-  if (etat_menu == 3) {
-    Serial.printf("Valeur TTL : <--",valTTL);
-    Serial.printf("-->");
-  }
-  else {
-    Serial.printf("Valeur TTL :",valTTL);
-  }
-}
-
-void message_envoie(uint16_t etat_menu, uint16_t status_send){
-  if (etat_menu == 4){
-    if (status_send == 0){
-        Serial.print("Envoyer : <-- Oui -->");
-    }
-    if (status_send == 1){
-      Serial.print("Envoyer : <-- Modifier -->");
-    }
-    if (status_send == 2){
-      Serial.print("Envoyer : <-- Non -->");
-    }
-  }
-  else {
-    if (status_send == 0){
-        Serial.print("Envoyer : Oui");
-    }
-    if (status_send == 1){
-      Serial.print("Envoyer : Modifier");
-    }
-    if (status_send == 2){
-      Serial.print("Envoyer : Non");
-    }
-  }
-}
-
-void Fonction_envoie_data_noTTL_noDest(uint16_t freq, uint16_t choixTTL);
-void Fonction_envoie_data_noTTL_Dest(uint16_t freq, uint16_t choixTTL, uint16_t ipm5, uint16_t self_ip);
-void Fonction_envoie_data_TTL_Dest(uint16_t freq, uint16_t choixTTL, uint16_t ipm5, uint16_t valTTL, uint16_t self_ip);
--- codes/Couche 3/Menu/test.ino
+++ codes/Couche 3/Menu/test.ino
@@ -2,185 +2,145 @@
 #include <RH_RF95.h>
 #include <M5Stack.h>
 
-#define BASE_FREQ 867.7
-#define BASE_CHOIX_TTL 0
-#define BASE_IP_DEST -1
-#define BASE_VAL_TTL -1
-
-#define MAX_FREQ 1000
-#define MIN_FREQ 600
-#define PAS_FREQ 10 //Valeur d'ajout/de soustraction de fréquance avec les boutons B et C
-
-#define NOMBRE_DESTINATAIRES 20 //Nombre maximum d'adresse dans le réseau
-
-#define TTL_MAX 32 //Nombre TTL max 
-
-void setup(){
-  // put your setup code here, to run once:
-  //Paramètres par défault
-  M5.begin();
+//Fonction d'affichage
+void affichage(uint8_t menu_data, uint16_t etat_menu, uint16_t freq, uint16_t choixTTL, uint16_t ipm5, uint16_t valTTL, uint16_t status_send){
+  if (menu_data == 0) {
+    M5.Lcd.clear();
+    printString("Etat initial");
+    termPutchar('\r');
+    sprintf(text, "Ip du m5 stack: %d", SELF_IP);
+    printString(text);
+    termPutchar('\r');
+  }
+  else {
+    M5.Lcd.clear();
+    if (choixTTL == 0){
+      message_choix_freq(etat_menu, freq);
+      message_choix_TTL(etat_menu, choixTTL);
+      message_envoie(etat_menu, status_send);
+    }
+    if (choixTTL == 1){
+      message_choix_freq(etat_menu, freq);
+      message_choix_TTL(etat_menu, choixTTL);
+      message_choix_dest(etat_menu, ipm5);
+      message_envoie(etat_menu, status_send);
+    }
+    if (choixTTL == 2){
+      message_choix_freq(etat_menu, freq);
+      message_choix_TTL(etat_menu, choixTTL);
+      message_choix_dest(etat_menu, ipm5);
+      message_choix_valTTL(etat_menu, valTTL);
+      message_envoie(etat_menu, status_send);
+    }
+  }
+  Serial.print("*****************************\n");
+  Serial.printf("Menu data : %d \n",menu_data);
+  Serial.printf("etat menu : %d\n",etat_menu);
+  Serial.printf("Freq : %d\n",freq);
+  Serial.printf("Choix TTL : %d\n",choixTTL);
+  Serial.printf("Ipdestm5 : %d\n",ipm5);
+  Serial.printf("Val TTL : %d\n",valTTL);
+  Serial.printf("Status send : %d\n",status_send);
 }
 
-void loop(){
-  M5.update();
+void message_choix_freq(uint16_t etat_menu, uint16_t freq){
+  if (etat_menu == 0) {
+    sprintf(text, "Frequance : <-- %d -->", freq);
+    printString(text);
+    termPutchar('\r');
+  }
+  else {
+    sprintf(text, "Frequance : %d", freq);
+    printString(text);
+    termPutchar('\r');
+  }
+}
 
-  bool menu_data = 0; //état du menu : 
-  //Si 0 alors menu de base avec juste écrit l'ip 
-  //Si 1 alors dans le menu d'envoie
-  uint16_t etat_menu = 0; //état de modification du menu : 
-  //Si 0 alors choix de frequence 
-  //Si 1 Choix TTL 
-  //Si 2 Choix du destinataire de TTL 
-  //Si 3 Choix du nombre de TTL 
-  //Si 4 envoie du packet
-
-  uint16_t freq=BASE_FREQ;
-  uint16_t choixTTL=0;
-  uint16_t ipm5=BASE_IP_DEST;
-  uint16_t valTTL=BASE_VAL_TTL;
-  uint16_t status_send=0;
-
-  uint16_t self_ip=4; //Valeur à changer par la vraie IP de notre M5 Stack
-
-  if(M5.BtnA.isPressed()){ //Si le bouton A est pressé
-    if (menu_data == 0){ //Si on est sur le menu d'acceuil
-      menu_data = 1;
-      affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
+void message_choix_TTL(uint16_t etat_menu, uint16_t choixTTL){
+  if (etat_menu == 1){
+    if (choixTTL == 0){
+      printString("Choix TTL : <-- Pas de TTL + Pas de destination -->");
+      termPutchar('\r');
     }
-    else {//sinon
-      if (etat_menu == 4){ //Moment d'envoyer le paquet
-        if (status_send == 0){ //Correspond à l'état "Envoyer"
-          if (choixTTL == 0){
-            Fonction_envoie_data_noTTL_noDest(freq, choixTTL);//Envoie la donnée sans TTL ni destination
-          }
-          if (choixTTL == 1){
-            Fonction_envoie_data_noTTL_Dest(freq, choixTTL, ipm5, self_ip);//Envoie la donnée sans TTL avec destination
-          }
-          if (choixTTL == 2){
-            Fonction_envoie_data_TTL_Dest(freq, choixTTL, ipm5, valTTL, self_ip);//Envoie la donnée avec TTL avec destination
-          }
-          menu_data = 0;
-          etat_menu = 0;
-          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);// Donc affichage du menu d'acceuil
-        }
-        if (status_send == 1){ //Correspond à l'état "modification"
-          etat_menu = 0;
-          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
-        }
-        else { //Annule les l'envoie de données
-          menu_data = 0;
-          etat_menu = 0;
-          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);// Donc affichage du menu d'acceuil
-        }
-      }
-      else {
-        if (etat_menu=0) {
-          etat_menu+=1;//On change de menu
-        }
-        else {
-          if (choixTTL == 0){
-            etat_menu=4;//Renvoie directement à l'envoie du paquet
-          }
-          if (choixTTL == 1){
-            if(etat_menu == 2){
-              etat_menu=4;
-            }
-            else {
-              etat_menu+=1;//On change de menu
-            }
-          }
-          if (choixTTL == 2){
-            etat_menu+=1;//On change de menu
-          }
-        }
-        affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
-      }
+    if (choixTTL == 1){
+      printString("Choix TTL : <-- Pas de TTL + Destination -->");
+      termPutchar('\r');
+    }
+    if (choixTTL == 2){
+      printString("Choix TTL : <-- TTL + Destination -->");
+      termPutchar('\r');
     }
   }
-
-  if(M5.BtnB.isPressed()){//Si le bouton B est pressé
-    if (menu_data == 1){//Si la création d'un packet est active
-      if (etat_menu == 0){ //Changement de fréquance
-        if (freq+PAS_FREQ < MAX_FREQ ){
-          freq+=PAS_FREQ;
-          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
-        }
-      }
-      if (etat_menu == 1){ //Choix du type d'envoie
-        if (choixTTL+1 <= 3){
-          choixTTL+=1;
-          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
-        }
-      }
-      if (etat_menu == 2){ //Choix du destinataire
-        if (ipm5+1 == self_ip){
-          if (ipm5+2 <= NOMBRE_DESTINATAIRES){
-            ipm5+=2;
-            affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
-          }
-        }
-        else{
-          if (ipm5+1 <= NOMBRE_DESTINATAIRES){
-            ipm5+=1;
-            affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
-          }
-        }
-      }
-      if (etat_menu == 3){ //Choix du TTL
-        if (valTTL+1 <= TTL_MAX){
-          valTTL+=1;
-          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
-        }
-      }
-      if (etat_menu == 4){ //Choix d'envoie de la donnée
-        if (status_send+1 <= 3){
-          status_send+=1;
-          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
-        }
-      }
+  else {
+    if (choixTTL == 0){
+      printString("Choix TTL : Pas de TTL + Pas de destination");
+      termPutchar('\r');
     }
-  }
-
-  if(M5.BtnC.isPressed()){//Si le bouton C est pressé
-    if (menu_data == 1){//Si la création d'un packet est active
-      if (etat_menu == 0){ //Changement de fréquance
-        if (freq-PAS_FREQ > MIN_FREQ ){
-          freq-=PAS_FREQ;
-          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
-        }
-      }
-      if (etat_menu == 1){ //Choix du type d'envoie
-        if (choixTTL-1 >= 0){
-          choixTTL-=1;
-          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
-        }
-      }
-      if (etat_menu == 2){ //Choix du destinataire
-        if (ipm5-1 == self_ip){
-          if (ipm5-2 >= 0){
-            ipm5-=2;
-            affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
-          }
-        }
-        else{
-          if (ipm5-1 >= 0){
-            ipm5-=1;
-            affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
-          }
-        }
-      }
-      if (etat_menu == 3){ //Choix du TTL
-        if (valTTL-1 >= 0){
-          valTTL-=1;
-          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
-        }
-      }
-      if (etat_menu == 4){ //Choix d'envoie de la donnée
-        if (status_send-1 >= 0){
-          status_send-=1;
-          affichage(menu_data, etat_menu, freq, choixTTL, ipm5, valTTL, status_send);
-        }
-      }
+    if (choixTTL == 1){
+      printString("Choix TTL : Pas de TTL + Destination");
+      termPutchar('\r');
+    }
+    if (choixTTL == 2){
+      printString("Choix TTL : TTL");
+      termPutchar('\r');
     }
   }
 }
+
+void message_choix_dest(uint16_t etat_menu, uint16_t ipm5){
+  if (etat_menu == 2) {
+    sprintf(text, "Destination : <-- %d -->", ipm5);
+    printString(text);
+    termPutchar('\r');
+  }
+  else {
+    sprintf(text, "Destination : %d", ipm5);
+    printString(text);
+    termPutchar('\r');
+  }
+}
+
+void message_choix_valTTL(uint16_t etat_menu, uint16_t valTTL){
+  if (etat_menu == 3) {
+    sprintf(text, "Valeur TTL : <-- %d -->", valTTL);
+    printString(text);
+    termPutchar('\r');
+  }
+  else {
+    sprintf(text, "Valeur TTL : %d", valTTL);
+    printString(text);
+    termPutchar('\r');
+  }
+}
+
+void message_envoie(uint16_t etat_menu, uint16_t status_send){
+  if (etat_menu == 4){
+    if (status_send == 0){
+      printString("Envoyer : <-- Oui -->");
+      termPutchar('\r');
+    }
+    if (status_send == 1){
+      printString("Envoyer : <-- Modifier -->");
+      termPutchar('\r');
+    }
+    if (status_send == 2){
+      printString("Envoyer : <-- Non -->");
+      termPutchar('\r');
+    }
+  }
+  else {
+    if (status_send == 0){
+      printString("Envoyer : Oui");
+      termPutchar('\r');
+    }
+    if (status_send == 1){
+      printString("Envoyer : Modifier");
+      termPutchar('\r');
+    }
+    if (status_send == 2){
+      printString("Envoyer : Non");
+      termPutchar('\r');
+    }
+  }
+}
+
